4d54ae47578d47e80fe423f359bd8146fe074203
David McKee <dragon@dxw.com>
2022-04-29 13:53:31 +0100

    WIP: only kinda-functional author emails

--- drivetime.py
+++ drivetime.py
@@ -84,14 +84,16 @@ def create_view():
   # very optional
   cur.execute("DROP VIEW IF EXISTS summary")
   cur.execute("""CREATE VIEW summary AS
-  SELECT webViewLink, drive_files.name as filename, displayName, emailAddress FROM
+  SELECT webViewLink, drive_files.name as filename, drive_users.displayName as reader_name, drive_users.emailAddress as reader_email, file_owners.displayName as author_name, file_owners.emailAddress as author_email FROM
   drive_users LEFT JOIN drive_permissions ON drive_users.permissionId=drive_permissions.id
               LEFT JOIN drive_files ON drive_permissions.fileID=drive_files.id
-  WHERE emailAddress NOT LIKE "%dxw.com"
-  GROUP BY name, displayName
+              LEFT JOIN drive_users AS file_owners ON drive_files._owner=drive_users.permissionId
+  WHERE drive_users.emailAddress NOT LIKE "%dxw.com"
+  GROUP BY name, drive_users.displayName
   """)
 
 create_view()
+exit()
 reset_permissions_table()
 get_permission_data(files)
 
